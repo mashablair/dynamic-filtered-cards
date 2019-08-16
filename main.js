0f4e37128ac9a220b47e90daf81500d75f72bb82
Masha Blair <mashablair@gmail.com>
2019-08-15 17:24:34 -0700

    Basic JS functionality is done -- build and render all cards from the data object.

--- main.js
+++ main.js
@@ -9,29 +9,56 @@ var myApp = (function() {
 
   // 1. display cards from data first
   // display all cards from the data
-  var CARD = `<div id="unit4801" class="availability-cards__card">
-                <div class="availability-cards__title">Apartment #<span class="availability-cards__unit-number">4801</span></div>
+
+  // Helper functions:
+  function _aptTypeToString(bed) {
+    if (bed === 0) {
+      return "Studio";
+    } else {
+      return bed + " Bed";
+    }
+  }
+
+  function _convertDateFormat(date) {
+    console.log("convertDateFormat called");
+
+    var newDate = new Date(date);
+    var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var newDateString = monthNames[newDate.getMonth()] + " " + newDate.getDate() + ", " + newDate.getFullYear();
+
+    return newDateString;
+  }
+
+  $.each(allUnits, function(unit, unitData) {
+    // for each item in allUnits data, create a card and fill out the info w/ data details
+    var CARD = `<div id="${unitData.unit_id}" class="availability-cards__card">
+                <div class="availability-cards__title">Apartment #<span class="availability-cards__unit-number">${unitData.unit_number}</span></div>
                 <div class="availability-cards__img">
                   <div class="availability-cards__no-img">
                     <i class="iconm-images darker-gray"></i>
                     <p class="darker-gray">No Image Available</p>
                   </div>
-                  <img src="https://cdn.rentcafe.com/dmslivecafe/3/19998/homegallery.jpg" alt="">
+                  <img src="${unitData.photos_unit[0]}" alt="">
                 </div>
                 <div class="availability-cards__details">
                   <p class="darker-gray">Floorplan <span class="availability-cards__floorplan">B1</span></p>
                   <div class="availability-cards__blocks m-t-sm m-b-sm">
-                    <p class="bigger-text"><span class="availability-cards__beds">2 Bed</span>, <span class="availability-cards__baths">2</span> Bath</p>
-                    <p class="darker-gray"><span class="availability-cards__sqft">1119</span> Sq Ft</p>
+                    <p class="bigger-text"><span class="availability-cards__beds">${_aptTypeToString(unitData.bedrooms)}</span>, <span class="availability-cards__baths">${
+      unitData.bathroom
+    }</span> Bath</p>
+                    <p class="darker-gray"><span class="availability-cards__sqft">${unitData.sq_ft}</span> Sq Ft</p>
                   </div>
                   <div class="availability-cards__blocks block-right">
-                    <p class="darker-gray">From <span class="bigger-text">$</span><span class="availability-cards__price bigger-text">7225</span></p>
-                    <p class="darker-gray">$<span class="availability-cards__sqft">12,000</span> Deposit</p>
+                    <p class="darker-gray">From <span class="bigger-text">$</span><span class="availability-cards__price bigger-text">${unitData.rent_price}</span></p>
+                    <p class="darker-gray">$<span class="availability-cards__sqft">${unitData.deposit}</span> Deposit</p>
                   </div>
-                  <p class="bigger-text">Available <span class="availability-cards__date-available">June 15, 2018</span></p>
+                  <p class="bigger-text">Available <span class="availability-cards__date-available">${_convertDateFormat(unitData.available)}</span></p>
                   <button class="availability-cards__btn m-t-md">UNIT DETAILS</button>
                 </div>
               </div>`;
+
+    cards.append(CARD);
+  });
 
   /**
    * Extend the public methods object
